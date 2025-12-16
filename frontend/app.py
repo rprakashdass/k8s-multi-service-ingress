398c621af6f6bd85618b5c58f0ac9811bcc8e3c4
rprakashdass <rprakashdass@gmail.com>
2025-12-16 11:19:27 +0530

    fix: set BACKEND_BASE_URL in frontend deployment and update links in app

--- frontend/app.py
+++ frontend/app.py
@@ -1,15 +1,18 @@
+import os
 from flask import Flask
+BACKEND_BASE_URL = os.getenv("BACKEND_BASE_URL")
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return """
-    <h1>Frontend App</h1>
-    <p>This is the frontend service</p>
-    <a href="/api">Call Backend</a>
-    <a href="/auth">Call Auth</a>
-    """
+    return  f"""
+            <h1>Frontend App</h1>
+            <p>This is the frontend service</p>
+            <a href="{BACKEND_BASE_URL}/api">Call Backend</a>
+            <a href="{BACKEND_BASE_URL}/auth">Call Auth</a>
+            """
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7000)
